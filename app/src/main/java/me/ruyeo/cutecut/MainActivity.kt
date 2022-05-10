186e4af99cc9b63d0a86b91e088b4a3e33bbb836
rustamsobirov <carwolt123@gmail.com>
2022-05-10 11:22:10 +0500

    initial commit

--- app/src/main/java/me/ruyeo/cutecut/MainActivity.kt
+++ app/src/main/java/me/ruyeo/cutecut/MainActivity.kt
@@ -2,10 +2,12 @@ package me.ruyeo.cutecut
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "ok", Toast.LENGTH_SHORT).show()
     }
 }
